3e731f531d410426dc6315603d7a0019f6b29976
Nkululeko Mbhele <nkululekombhele6017@gmail.com>
2022-04-19 20:47:49 +0200

    splash screen started

---
+++ lib/splash.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+            child: Column(
+            children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 80.0),
+                    child: Image.asset('assets/images/logo.jpg'),
+                ),
+                Text(
+              "Time o'clock",
+                ),
+                Text(
+                'Loading...',
+                style: Theme.of(context).textTheme.headline4,
+                ),
+          ],
+        ),
+      )
+      )
+    );
+  }
+}
